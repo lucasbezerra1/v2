4656959b516bca858607d969dcc8293197fc4aa2
lucasbezerra1 <92055652+lucasbezerra1@users.noreply.github.com>
2021-10-07 13:46:03 -0300

    Update main.js

--- Sound_controlled_alien/main.js
+++ Sound_controlled_alien/main.js
@@ -17,8 +17,8 @@ function gotResults(error, results) {
     random_number_g = Math.floor(Math.random() * 255) + 1;
     random_number_b = Math.floor(Math.random() * 255) + 1;
 
-    document.getElementById("result_label").innerHTML = 'I can hear - '+ results[0].label;
-    document.getElementById("result_confidence").innerHTML = 'Accuracy - '+ (results[0].confidence*100).toFixed(2)+" %";
+    document.getElementById("result_label").innerHTML = 'Eu posso ouvir - '+ results[0].label;
+    document.getElementById("result_confidence").innerHTML = 'Precisão - '+ (results[0].confidence*100).toFixed(2)+" %";
     document.getElementById("result_label").style.color = "rgb("+random_number_r+","+random_number_g+","+random_number_r+")";
     document.getElementById("result_confidence").style.color = "rgb("+random_number_r+","+random_number_g+","+random_number_r+")";
 
